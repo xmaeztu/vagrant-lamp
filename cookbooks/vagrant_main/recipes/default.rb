1f8c95a4f58502b875cfe1fe5238fc8b9d944937
Xabier Maeztu <x.maeztu@gmail.com>
2013-05-09 17:32:44 +0100

    Create symlinks

--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -2,9 +2,10 @@ include_recipe "apt"
 include_recipe "apache2"
 include_recipe "mysql::server"
 include_recipe "php::php5"
+include_recipe "drush"
 
 # Some neat package (subversion is needed for "subversion" chef ressource)
-%w{ debconf php5-xdebug subversion  }.each do |a_package|
+%w{ debconf php5-xdebug }.each do |a_package|
   package a_package
 end
 
@@ -12,12 +13,19 @@ end
 cookbook_file "/tmp/phpmyadmin.deb.conf" do
   source "phpmyadmin.deb.conf"
 end
+
 bash "debconf_for_phpmyadmin" do
   code "debconf-set-selections /tmp/phpmyadmin.deb.conf"
 end
+
 package "phpmyadmin"
 
-s = "xabi.quality-bmj-com.internal.bmjgroup.com"
+execute "install_drupal_root" do
+  command "drush dl drupal -y --drupal-project-rename='#{node[:vagrant_main][:site_alias]}' --destination='/vagrant'"
+  not_if "find /vagrant -maxdepth 1 | grep /vagrant/#{node[:vagrant_main][:site_alias]}"
+end
+
+s = node[:vagrant_main][:site_name]
 site = {
   :name => s, 
   :host => s, 
@@ -25,25 +33,18 @@ site = {
 }
 
 # Configure the development site
-#web_app site[:name] do
-#  template "sites.conf.erb"
-#  server_name site[:host]
-#  server_aliases site[:aliases]
-#  docroot "/vagrant/public/"
-#end  
+web_app site[:name] do
+  template "sites.conf.erb"
+  server_name site[:host]
+  server_aliases site[:aliases]
+  docroot "/vagrant/#{node[:vagrant_main][:site_alias]}"
+end  
 
 # Add site info in /etc/hosts
 bash "info_in_etc_hosts" do
   code "echo 127.0.0.1 #{site[:host]} #{site[:aliases]} >> /etc/hosts"
 end
 
-## Retrieve webgrind for xdebug trace analysis
-#subversion "Webgrind" do
-#  repository "http://webgrind.googlecode.com/svn/trunk/"
-#  revision "HEAD"
-#  destination "/var/www/webgrind"
-#  action :sync
-#end
 
 # Add an admin user to mysql
 execute "add-admin-user" do
@@ -56,4 +57,29 @@ execute "add-admin-user" do
   action :run
   only_if { `/usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} -D mysql -r -N -e \"SELECT COUNT(*) FROM user where user='myadmin' and host='localhost'"`.to_i == 0 }
   ignore_failure true
+end
+
+directory "/vagrant/#{node[:vagrant_main][:site_alias]}/sites/mainline" do
+  owner "vagrant"
+  group "vagrant"
+  mode "0755"
+  action :create
+end
+
+link "/vagrant/#{node[:vagrant_main][:site_alias]}/sites/#{node[:vagrant_main][:site_alias]}" do
+  to "/vagrant/#{node[:vagrant_main][:site_alias]}/sites/mainline"
+end
+
+link "/vagrant/#{node[:vagrant_main][:site_alias]}/sites/#{node[:vagrant_main][:site_name]}" do
+  to "/vagrant/#{node[:vagrant_main][:site_alias]}/sites/mainline"
+end
+
+execute "create-db" do
+  command "/usr/bin/mysqladmin -u root -p#{node[:mysql][:server_root_password]} create #{node[:vagrant_main][:db_name]}"
+  not_if "/usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} -r -N -e \"SHOW databases;\" | grep #{node[:vagrant_main][:db_name]}"
+end
+
+execute "import-db" do
+  command "/usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} #{node[:vagrant_main][:db_name]} < /vagrant/#{node[:vagrant_main][:db_name]}.sql"
+  not_if "/usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} -D #{node[:vagrant_main][:db_name]} -r -N -e \"SELECT COUNT(*) FROM system;\""
 end
