2a555f1733205ca00406f692a742ea5525d57390
Xabier Maeztu <trisketonni@gmail.com>
2013-09-15 18:39:55 +0200

    Changes to recipes

--- Vagrantfile
+++ Vagrantfile
@@ -1,24 +1,26 @@
-Vagrant::Config.run do |config|
+Vagrant.configure("2") do |config|
   # All Vagrant configuration is done here. For a detailed explanation
   # and listing of configuration options, please view the documentation
   # online.
 
   # Every Vagrant virtual environment requires a box to build off of.
-  config.vm.box = "base"
-  #config.vm.box_url = "http://files.vagrantup.com/lucid32.box"
+  config.vm.box = "precise32"
+  config.vm.box_url = "http://files.vagrantup.com/precise32.box"
+  
+  config.vm.synced_folder ".", "/vagrant", :nfs => true, :extra => "dmode=777,fmode=666"
 
-  #config.vm.provision :shell, :path => "vagrant-setup.sh"
+  config.vm.provision :shell, :path => "vagrant-setup.sh"
   config.vm.provision :chef_solo do |chef|
     chef.cookbooks_path = "cookbooks"
     chef.add_recipe("vagrant_main")
-    chef.add_recipe("drush")
+    chef.add_recipe("vim")
     chef.json.merge!({
     :mysql => {
       :server_root_password => "root"
     }
   })
   end
-
-  config.vm.forward_port(80, 8080)
+  config.vm.network :private_network, ip: "192.168.2.222"
+  config.vm.network :forwarded_port, guest: 80, host: 8080
 
 end
--- cookbooks/vagrant_main/recipes/default.rb
+++ cookbooks/vagrant_main/recipes/default.rb
@@ -15,28 +15,41 @@ end
 
 package "phpmyadmin"
 
-execute "install_drupal_root" do
-  command "drush dl drupal -y --drupal-project-rename='drupal#{node[:vagrant_main][:drupal_version]}' --destination='/vagrant'"
-  not_if "find /vagrant -maxdepth 1 | grep /vagrant/drupal#{node[:vagrant_main][:drupal_version]}"
-end
+#execute "install_drupal_root" do
+#  command "drush dl drupal -y --drupal-project-rename='drupal#{node[:vagrant_main][:drupal_version]}' --destination='/vagrant'"
+#  not_if "find /vagrant -maxdepth 1 | grep /vagrant/drupal#{node[:vagrant_main][:drupal_version]}"
+#end
 
 site = {
   :name => node[:vagrant_main][:site_name], 
   :host => node[:vagrant_main][:site_name], 
-  #:aliases => ["#{s}.com", "dev.#{s}-static.com"]
+  :alias => "www.#{node[:vagrant_main][:site_name]}"
 }
 
 bash "info_in_etc_hosts" do
-  code "echo 127.0.0.1 #{site[:host]} >> /etc/hosts"
+  code "echo 127.0.0.1 #{site[:host]} www.#{site[:host]} >> /etc/hosts"
 end
 
 # Configure the development site
 web_app site[:name] do
   template "sites.conf.erb"
   server_name site[:host]
-  #server_aliases site[:aliases]
-  docroot "/vagrant/#{node[:vagrant_main][:site_alias]}"
+  server_alias site[:alias]
+  docroot "/vagrant/#{node[:vagrant_main][:site_name]}"
 end  
+
+# Add an admin user to mysql
+execute "add-admin-user" do
+  command "/usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} -e \"" +
+      "CREATE USER 'myadmin'@'localhost' IDENTIFIED BY 'myadmin';" +
+      "GRANT ALL PRIVILEGES ON *.* TO 'myadmin'@'localhost' WITH GRANT OPTION;" +
+      "CREATE USER 'myadmin'@'%' IDENTIFIED BY 'myadmin';" +
+      "GRANT ALL PRIVILEGES ON *.* TO 'myadmin'@'%' WITH GRANT OPTION;\" " +
+      "mysql"
+  action :run
+  only_if { `/usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} -D mysql -r -N -e \"SELECT COUNT(*) FROM user where user='myadmin' and host='localhost'"`.to_i == 0 }
+  ignore_failure true
+end
 
 #
 ## Add an admin user to mysql
